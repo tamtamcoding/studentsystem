b66dac1c6479d2cac53cc9f338fd18c4e5e4f335
Arifin <rustam.arifin@accenture.com>
2022-10-04 14:56:03 +0800

    Initial commit

---
+++ src/main/java/com/rustamproject/studentsystem/model/Student.java
@@ -0,0 +1,52 @@
+package com.rustamproject.studentsystem.model;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name ="firstname")
+    private String firstName;
+    @Column(name = "lastname")
+    private  String lastName;
+    @Column(name="email")
+    private String email;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Student() {
+    }
+
+    public Student(String firstName, String lastName, String email) {
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+}
